e7d18f900ff47ecd6175fb669b7f1eb1035c7047
shaswatmisra2310 <shaswatmisra2310@gmail.com>
2022-08-08 17:37:45 +0530

    Add project files.

---
+++ ZeissWeb.Welcome/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZeissWeb.Welcome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
